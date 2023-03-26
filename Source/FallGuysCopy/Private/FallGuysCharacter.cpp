c3736b3d52f7012342543e1eb6b11c560f94f747
Limjiyoung <22314671@naver.com>
2023-03-27 01:18:24 +0900

    플레이어 이동

--- Source/FallGuysCopy/Private/FallGuysCharacter.cpp
+++ Source/FallGuysCopy/Private/FallGuysCharacter.cpp
@@ -2,12 +2,32 @@
 
 
 #include "FallGuysCharacter.h"
+#include <EnhancedInputSubsystems.h>
+#include <Engine/LocalPlayer.h>
+#include <FallGuysGameModeBase.h>
+#include <EnhancedInputComponent.h>
+#include <Camera/CameraComponent.h>
+#include <GameFramework/SpringArmComponent.h>
+#include <GameFramework/Character.h>
+#include <Components/CapsuleComponent.h>
+
 
 // Sets default values
 AFallGuysCharacter::AFallGuysCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	GetCapsuleComponent()->SetCollisionProfileName(TEXT("PlayerPreset"));
+
+
+	springArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("springArm"));
+	springArm->SetupAttachment(RootComponent);
+	springArm->bUsePawnControlRotation = true;
+	cameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("cameraComp"));
+	cameraComp->SetupAttachment(springArm);
+	cameraComp->bUsePawnControlRotation = true;
+
 
 }
 
@@ -16,6 +36,21 @@ void AFallGuysCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	auto PC = Cast<APlayerController>(GetWorld()->GetFirstPlayerController());
+
+	if (PC)
+	{
+		auto localPlayer = PC->GetLocalPlayer();
+		auto subSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(localPlayer);
+		if (subSystem)
+		{
+			subSystem->AddMappingContext(IMC_ChaInput,0);
+		}
+	}
+
+	GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &AFallGuysCharacter::OnOverlap);
+
+
 }
 
 // Called every frame
@@ -30,5 +65,60 @@ void AFallGuysCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	auto InputSystem = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (InputSystem)
+	{
+		InputSystem->BindAction(IA_ChaMove, ETriggerEvent::Triggered, this, &AFallGuysCharacter::Move);
+		InputSystem->BindAction(IA_ChaLook, ETriggerEvent::Triggered, this, &AFallGuysCharacter::Look);
+		InputSystem->BindAction(IA_ChaJump, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		InputSystem->BindAction(IA_ChaJump, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &AFallGuysCharacter::Jumping);
+		PlayerInputComponent->BindAction(TEXT("Hold"), IE_Pressed, this, &AFallGuysCharacter::Holding);
+
+	}
+}
+
+void AFallGuysCharacter::Move(const FInputActionValue& Values)
+{
+	FVector2D Axis = Values.Get<FVector2D>();
+	AddMovementInput(GetActorForwardVector(), Axis.X);
+	AddMovementInput(GetActorRightVector(), Axis.Y);
+}
+
+void AFallGuysCharacter::Look(const FInputActionValue& Values)
+{
+
+	FVector2D Axis = Values.Get<FVector2D>();
+	AddControllerYawInput(Axis.X);
+	AddControllerPitchInput(Axis.Y);
+
+}
+
+void AFallGuysCharacter::Jumping()
+{
+	Jump();
+}
+
+void AFallGuysCharacter::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	isOverlap = true;
+
+}
+
+void AFallGuysCharacter::Holding()
+{
+	
+	UE_LOG(LogTemp, Warning, TEXT("Hold"));
+	if (isOverlap == true)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("OverLapHold"));
+
+	}
+	else
+	{
+		return;
+	}
+	
 }
 
--- Source/FallGuysCopy/Public/FallGuysCharacter.h
+++ Source/FallGuysCopy/Public/FallGuysCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include <InputActionValue.h>
 #include "FallGuysCharacter.generated.h"
 
 UCLASS()
@@ -26,6 +27,34 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere,Category = "Input")
+	class UInputMappingContext* IMC_ChaInput;
 
+	UPROPERTY(EditAnywhere,Category = "Input")
+	class UInputAction* IA_ChaMove;
+	UPROPERTY(EditAnywhere,Category = "Input")
+	class UInputAction* IA_ChaLook;
+	UPROPERTY(EditAnywhere,Category = "Input")
+	class UInputAction* IA_ChaJump;
+
+	UPROPERTY(EditAnywhere)
+	class UCameraComponent* cameraComp;
+	UPROPERTY(EditAnywhere)
+	class USpringArmComponent* springArm;
+	
+	//잡기범위
+	UPROPERTY(EditAnywhere, Category = "Hold")
+	float HoldRange = 50;
+
+	void Move(const FInputActionValue& Values);
+	void Look(const FInputActionValue& Values);
+	void Jumping();
+	//잡기
+	UFUNCTION()
+	void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void Holding();
+	
+	
+	bool isOverlap = false;
 
 };
