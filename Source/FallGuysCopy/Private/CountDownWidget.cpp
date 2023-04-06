b2c80d7fd8ff626b4f511cc1d25ddf9f0a62a37c
Jellyforest <22314671@naver.com>
2023-04-06 13:32:21 +0900

    4.06.목요일

---
+++ Source/FallGuysCopy/Private/CountDownWidget.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CountDownWidget.h"
+#include "Components/TextBlock.h"
+
+void UCountDownWidget::NativeConstruct()
+{
+	//text_CountNumber-> 
+	//FString 
+}
